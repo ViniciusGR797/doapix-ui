e17a4c449aedcf63199f520077e612a30d318f8c
mrjonas151 <jonas.aquinos@hotmail.com>
2024-03-25 15:34:40 -0300

    Feat: Tela de login finalizada

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,33 +26,36 @@ export default function Home() {
       </Head>
 
       <div className={styles.mainDiv}>
-        <div className={styles.Photo}>
-          <Image src={children} alt="children-smiling"/>
-        </div>
         
-          <div className={styles.containerCenter}>
-            <Image src={logo} alt="doa-pix logo" height={200}/>
-            <div className={styles.login}>
-              <form className={styles.form}>
-                  <h1>Login</h1>
-                  <Input placeholder="Email" type="text" value={email} onChange={ (e) => setEmail(e.target.value)} />
-                  <Input placeholder="Senha" type="password" value={password} onChange={ (e) => setPassword(e.target.value)} />
-                  <Button type="submit" loading={false} style={{ marginTop: '4%' }}>Entrar</Button>
-                  <a>Ou continue como</a>
-                  <button className={styles.buttonAnonimo}>Doador anonimo</button>
 
-                  <div className={styles.linha}>
-                    <button className={styles.touchableOpacity}> <FiUser size={20} style={{ verticalAlign: 'middle', marginBottom: '1px' }}/> Criar Conta</button>
-                    <button className={styles.touchableOpacity}> <FiMail size={20} style={{ verticalAlign: 'middle', marginBottom: '2px' }}/> Esqueci senha</button>
-                  </div>
-              </form>
-            </div>
-            </div>
-
-          <div className={styles.Photo}>
-            <Image src={hands} alt="hands"/>
+        <div className={styles.containerCenter}>
+          <Image src={logo} alt="doa-pix logo" height={200}/>
+          <div className={styles.login}>
+            <form className={styles.form}>
+              <h1>Login</h1>
+              <Input placeholder="Email" type="text" value={email} onChange={ (e) => setEmail(e.target.value)} />
+              <Input placeholder="Senha" type="password" value={password} onChange={ (e) => setPassword(e.target.value)} />
+              <Button type="submit" loading={false} style={{ marginTop: '4%' }}>Entrar</Button>
+              <a>Ou continue como</a>
+              <button className={styles.buttonAnonimo}>Doador anonimo</button>
+              <div className={styles.linha}>
+                <button className={styles.touchableOpacity}> <FiUser size={20} style={{ verticalAlign: 'middle', marginBottom: '1px' }}/> Criar Conta</button>
+                <button className={styles.touchableOpacity}> <FiMail size={20} style={{ verticalAlign: 'middle', marginBottom: '2px' }}/> Esqueci senha</button>
+              </div>
+            </form>
           </div>
         </div>
+        
+        <div className={styles.rightBackground}>
+          <Image src={hands} alt="hands" layout="fill" objectFit="cover" />
+        </div>
+
+        <div className={styles.leftBackground}>
+          <Image src={children} alt="children-smiling" layout="fill" objectFit="cover" />
+        </div>
+
+        
+      </div>
     </>
   );
 }
